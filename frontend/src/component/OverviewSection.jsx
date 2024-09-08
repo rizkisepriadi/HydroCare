fcf802ce25966ce59a75ea150c1e2f1f80f23118
Haruki <jimmymaulana01@gmail.com>
2024-09-08 15:04:45 +0700

    OverviewSection

---
+++ frontend/src/component/OverviewSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import Logo from "../assets/logo.svg";
+
+export default function OverviewSection() {
+  return (
+    <div className="fixed flex justify-between items-center pl-3 bg-[#A1C3DB] w-screen">
+
+    </div>
+  );
+}
