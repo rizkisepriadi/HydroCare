e3b6233ce4a046ccafee6b33a578665f2140995c
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-09-08 17:10:55 +0700

    chore: Update Button and Navbar components

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import Navbar from '../component/Navbar.jsx'
+import React from "react";
+import Navbar from "../component/Navbar.jsx";
 
 export default function Homepage() {
   return (
-    <div>
+    <div className="">
       <Navbar />
-      <section  className= "bg-[url('/src/assets/HOME.png')] bg-cover bg-center h-screen"> {/* section 1 */}
-        <div className="flex items-center justify-center h-full"> {/* container */}
-          <div> 
-              <div className="flex items-center justify-center pb-5">
-              <p className='text-center text-xl text-[#0063A7] font-medium text-shadow-custom'>Bersama Menciptakan Dunia Dengan Air Bersih</p>
+      <section className="bg-[url('/src/assets/HOME.svg')] bg-cover bg-center h-screen">
+        {/* section 1 */}
+        <div className="flex items-center justify-center h-full">
+          {/* container */}
+          <div>
+            <div className="flex items-center justify-center pb-5">
+              <p className="text-center text-xl text-[#0063A7] font-medium text-shadow-custom">
+                Bersama Menciptakan Dunia Dengan Air Bersih
+              </p>
             </div>
             <div className="flex items-center justify-center pb-5">
               <div className="flex items-center justify-center">
-                <h1 className='text-center text-8xl tracking-[0.2em] font-extrabold text-[#0063A7] text-shadow-custom'>HydroCare</h1>
+                <h1 className="text-center text-8xl tracking-[0.2em] font-extrabold text-[#0063A7] text-shadow-custom">
+                  HydroCare
+                </h1>
               </div>
             </div>
-            <div className="flex items-center justify-center pb-5"> 
-              <p className='text-center text-xl text-[#0063A7] font-medium text-shadow-custom'>Kita Ubah Setiap Tetes Air Menjadi Harapan Baru <br></br> Bagi Mereka yang Kesulitan Mendapatkan Air Bersih.Mari Bergerak untuk<br></br> Masa Depan yang Lebih Baik.</p>
+            <div className="flex items-center justify-center pb-5">
+              <p className="text-center text-xl text-[#0063A7] font-medium text-shadow-custom">
+                Kita Ubah Setiap Tetes Air Menjadi Harapan Baru <br></br> Bagi
+                Mereka yang Kesulitan Mendapatkan Air Bersih.Mari Bergerak untuk
+                <br></br> Masa Depan yang Lebih Baik.
+              </p>
             </div>
           </div>
         </div>
-        
       </section>
     </div>
-  )
+  );
 }
