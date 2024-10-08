85359db584c0ed637bd7f9d333c4162ae2af9462
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-10-08 19:35:05 +0700

    Refactor blogCard component styles and structure

--- frontend/src/component/blogCard.jsx
+++ frontend/src/component/blogCard.jsx
@@ -3,22 +3,19 @@ import BlogImage from "../assets/blog.svg";
 
 export default function blogCard() {
   return (
-    <div className="flex bg-base-200 rounded-[20px] items-center shadow-md shadow-blue-300">
-      <div className="pl-3 w-1/2 h-full">
-        <img src={BlogImage} alt="" className="rounded-3xl" />
+    <div className="flex rounded-[20px] items-center border border-blue-300 shadow-lg shadow-indigo-300">
+      <div className="pl-3 w-[170px] h-[140px]">
+        <img src={BlogImage} alt="" className="w-full h-full" />
       </div>
-      <div className="text-primary p-3 w-1/2">
-        <p className="font-bold text-xs">19 Jan 2022</p>
-        <h1 className="text-sm font-extrabold text-wrap">
-          Cara Untuk Melakukan Penghematan Air di Rumah - HydroCare{" "}
-        </h1>
-        <p className="font-normal text-xs text-wrap">
+      <div className="text-primary px-3 py-2 w-1/2">
+        <p className="font-bold text-[10px] ">19 Jan 2022</p>
+        <a href="/article" className="text-sm font-extrabold text-wrap">
+          Cara Untuk Melakukan Penghematan Air di Rumah - HydroCare
+        </a>
+        <p className="font-normal text-[10px] text-wrap">
           Menjadi seorang programmer saat ini sudah sangat mudah untuk
           dipelajari oleh siapapun, da....
         </p>
-        <a href="" className="text-xs font-normal flex justify-end">
-          Selengkapnya
-        </a>
       </div>
     </div>
   );
