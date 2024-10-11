f726a5227023ae356dd4d98ba6dc390e36c898fc
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-10-11 23:11:56 +0700

    Refactor backend index.js to include user, post, and campaign routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import userRoute from "./routes/userRoute.js"
+import postRoute from "./routes/postRoute.js"
+import campaignRoute from "./routes/campaignRoute.js"
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,6 +16,10 @@ app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Hy, your HydroCare is connected");
 });
+
+app.use("/", userRoute)
+app.use("/", postRoute)
+app.use("/", campaignRoute)
 
 mongoose
   .connect(process.env.MONGO_URI)
