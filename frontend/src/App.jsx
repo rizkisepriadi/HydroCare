51fde4489f949139a7a9f029e4fbcbbba3c52533
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-10-12 18:10:53 +0700

    Add Campaign page to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import Article from "./pages/article.jsx";
+import Campaign from "./pages/campaign.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />}/>
       <Route path="/article" element={<Article />}/>
+      <Route path="/campaign" element={<Campaign />}/>
     </Routes>
   );
 }
