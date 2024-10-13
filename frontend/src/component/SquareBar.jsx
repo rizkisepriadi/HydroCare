286e88da973644429aab8d2c79ed31d7db6e86e6
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-10-13 21:51:16 +0700

    Refactor SquareBar component to update styling and responsiveness

--- frontend/src/component/SquareBar.jsx
+++ frontend/src/component/SquareBar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function SquareBar({ Number, Text, className = ""}) {
+export default function SquareBar({ Number, Text, className = "" }) {
   return (
     <div
-      className={`flex bg-primary text-white justify-center items-center gap-9 w-[325px] h-[76.7px] rounded-md ${className}`}
+      className={`flex bg-primary text-white justify-center items-center gap-9 lg:gap-5 w-[325px] h-[76.7px] lg:w-[290px] xl:w-[400px] md:w-full md:justify-start rounded-md ${className}`}
     >
-      <h1 className="text-3xl pl-8 font-extrabold">{Number}</h1>
-      <p className="text-sm py-2 pr-2">{Text}</p>
+      <h1 className="text-3xl pl-8 lg:pl-6 font-extrabold">{Number}</h1>
+      <p className="text-sm md:text-base py-2 pr-2 lg:text-sm xl:text-base">{Text}</p>
     </div>
   );
 }
--- frontend/src/pageSection/articleSection.jsx
+++ frontend/src/pageSection/articleSection.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 import BlogCard from "../component/blogCard.jsx";
-
 
 export default function article() {
   return (
-    <div className="flex flex-col px-[30px]">
-    <h1 className="text-primary text-center font-extrabold text-xl">
+    <div className="flex flex-col px-[10%]">
+      <h1 className="text-primary text-xl font-extrabold text-center xl:pt-20 lg:mb-5 lg:pt-10 lg:pb-0 py-10 md:text-2xl">
       Article
-    </h1>
-    <div className="pt-9 flex flex-col pb">
-      <a href="#" className="flex justify-end text-primary pb-2">
-        Lihat Semua
-      </a>
-      <div className="flex flex-col gap-5">
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
+      </h1>
+      <div className="flex flex-col">
+        <a href="#" className="flex justify-end text-primary pb-2">
+          Lihat Semua
+        </a>
+        <div className="flex flex-col lg:flex-row lg:justify-center gap-5 lg:gap-4 xl:gap-5">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          </div>
       </div>
     </div>
-  </div>  )
+  );
 }
