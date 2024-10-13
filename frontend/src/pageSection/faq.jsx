bc4e5ce778f2d341c7c623ba6b0e6cc674ce41ea
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-10-13 21:49:50 +0700

    Refactor campaignSection.jsx and faq.jsx components

--- frontend/src/pageSection/faq.jsx
+++ frontend/src/pageSection/faq.jsx
@@ -1,88 +1,75 @@
-import Open from "../assets/openIcon.svg";
-import Close from "../assets/closeIcon.svg";
-import { useState } from "react";
-import { useMediaQuery } from "@uidotdev/usehooks";
 
 export default function FAQ() {
-  const [openIndex, setOpenIndex] = useState(null);
-  const isLargeScreen = useMediaQuery("(max-width: 768px)");
-
-  // Ubah data menjadi array
-  const data = [
-    {
-      pertanyaan: "Apa itu Kampanye Peduli Air Bersih?",
-      jawaban:
-        "Kampanye Peduli Air Bersih adalah inisiatif global yang bertujuan meningkatkan kesadaran dan aksi nyata dalam menyediakan akses air bersih untuk semua orang, terutama mereka yang hidup di daerah kekurangan air. Kami fokus pada edukasi, konservasi, dan penggalangan dana untuk membantu masyarakat yang membutuhkan.",
-    },
-    {
-      pertanyaan: "Mengapa air bersih sangat penting?",
-      jawaban:
-        "Air bersih adalah kebutuhan dasar yang vital bagi kesehatan dan kehidupan manusia. Kekurangan air bersih dapat menyebabkan berbagai masalah kesehatan, seperti penyakit diare, kolera, dan lainnya. Dengan memastikan akses air bersih, kita membantu meningkatkan kualitas hidup dan kesehatan masyarakat secara keseluruhan.",
-    },
-    {
-      pertanyaan: "Bagaimana cara saya bisa berkontribusi?",
-      jawaban:
-        "Anda bisa berkontribusi dengan berbagai cara, seperti berdonasi, menjadi sukarelawan, atau menyebarkan informasi tentang pentingnya air bersih. Setiap kontribusi, sekecil apapun, sangat berarti dalam upaya kami mencapai tujuan kampanye ini.",
-    },
-    {
-      pertanyaan: "Ke mana dana donasi akan disalurkan?",
-      jawaban:
-        "Dana donasi akan digunakan untuk mendukung proyek-proyek yang menyediakan akses air bersih, seperti pembangunan sumur, penyediaan filter air, dan program edukasi di daerah-daerah yang membutuhkan. Semua dana dikelola secara transparan, dan laporan penggunaan dana dapat diakses oleh donatur.",
-    },
-  ];
-
-  const toggleOpen = (index) => {
-    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
-  };
-
   return (
-    <div className="flex flex-col lg:flex-row text-primary px-[30px] lg:px-[70px] lg:pt-[157px] lg:pb-[150px] gap-4 lg:gap-[136px] py-16">
-      <div className="flex flex-col items-center lg:items-start justify-center lg:w-[328px]">
-        <h1 className="text-xl lg:text-[28px] font-extrabold">
+    <div className="flex flex-col lg:flex-row text-primary lg:py-20 gap-4 py-16 px-[10%]">
+      <div className="flex flex-col items-center lg:items-start justify-center lg:w-1/3 xl:w-2/5">
+        <h1 className="text-primary text-xl font-extrabold text-center md:text-2xl lg:text-left xl:text-3xl">
           Frequently Asked Questions
         </h1>
-        <p className="text-xs font-normal lg:text-base opacity-70 lg:pt-2">
+        <p className="text-sm font-normal md:text-base opacity-70 lg:pt-2 xl:text-lg">
           Masih bingung atau ragu?
         </p>
       </div>
-      <div>
-        <div className="flex flex-col gap-4 lg:gap-[18px] lg:w-[817px]">
-          {data.map((item, index) => (
-            <div className="w-full" key={index}>
-              <div className="flex lg:gap-[59px] gap-4">
-                <p className="text-primary text-center lg:text-2xl font-medium">
-                  {`0${index + 1}`}
-                </p>
-                <div className="flex w-full justify-between">
-                  <h1
-                    className="lg:text-2xl font-bold text-primary lg:pr-[239px]"
-                    onClick={() => toggleOpen(index)}
-                  >
-                    {item.pertanyaan}
-                  </h1>
-                  {!isLargeScreen && (
-                    <button
-                      className="active:animate-spin"
-                      onClick={() => toggleOpen(index)}
-                    >
-                      {openIndex === index ? (
-                        <img src={Close} alt="" />
-                      ) : (
-                        <img src={Open} alt="" />
-                      )}
-                    </button>
-                  )}
-                </div>
-              </div>
-              <p
-                className={`${
-                  openIndex === index ? "static" : "hidden"
-                } lg:pl-[83px] pl-8 text-wrap text-xs lg:text-[18px] lg:w-[624px] lg:pt-[18px] font-medium`}
-              >
-                {item.jawaban}
-              </p>
-            </div>
-          ))}
+      <div className="lg:w-3/4 xl:w-3/5">
+        <div className="collapse collapse-arrow">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-md lg:text-lg xl:text-xl font-medium">
+            Apa itu Kampanye Peduli Air Bersih?
+          </div>
+          <div className="collapse-content text-md lg:text-lg xl:text-xl ">
+            <p>
+              Kampanye Peduli Air Bersih adalah inisiatif global yang bertujuan
+              meningkatkan kesadaran dan aksi nyata dalam menyediakan akses air
+              bersih untuk semua orang, terutama mereka yang hidup di daerah
+              kekurangan air. Kami fokus pada edukasi, konservasi, dan
+              penggalangan dana untuk membantu masyarakat yang membutuhkan.,
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-md lg:text-lg xl:text-xl font-medium">
+            Mengapa air bersih sangat penting?
+          </div>
+          <div className="collapse-content text-md lg:text-lg xl:text-xl ">
+            <p>
+              Air bersih adalah kebutuhan dasar yang vital bagi kesehatan dan
+              kehidupan manusia. Kekurangan air bersih dapat menyebabkan
+              berbagai masalah kesehatan, seperti penyakit diare, kolera, dan
+              lainnya. Dengan memastikan akses air bersih, kita membantu
+              meningkatkan kualitas hidup dan kesehatan masyarakat secara
+              keseluruhan.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-md lg:text-lg xl:text-xl font-medium">
+            Bagaimana cara saya bisa berkontribusi?
+          </div>
+          <div className="collapse-content text-md lg:text-lg xl:text-xl ">
+            <p>
+              Anda bisa berkontribusi dengan berbagai cara, seperti berdonasi,
+              menjadi sukarelawan, atau menyebarkan informasi tentang pentingnya
+              air bersih. Setiap kontribusi, sekecil apapun, sangat berarti
+              dalam upaya kami mencapai tujuan kampanye ini.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-md lg:text-lg xl:text-xl  font-medium">
+            Ke mana dana donasi akan disalurkan?
+          </div>
+          <div className="collapse-content text-md lg:text-lg xl:text-xl ">
+            <p>
+              Dana donasi akan digunakan untuk mendukung proyek-proyek yang
+              menyediakan akses air bersih, seperti pembangunan sumur,
+              penyediaan filter air, dan program edukasi di daerah-daerah yang
+              membutuhkan. Semua dana dikelola secara transparan, dan laporan
+              penggunaan dana dapat diakses oleh donatur.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pageSection/footer.jsx
+++ frontend/src/pageSection/footer.jsx
@@ -6,23 +6,32 @@ import Line from "../assets/line.svg";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col bg-base-200 items-center text-primary mb-6">
-      <img src={Logo} alt="" className="w-[38.489px] pt-6" />
-      <h1 className="text-lg font-medium text-center pt-4">
-        Bersama Menciptakan Dunia dengan Air Bersih untuk Semua.{" "}
-      </h1>
-      <div className="flex pt-8 gap-24">
+    <div className="flex flex-col lg:flex-row bg-base-200 lg:justify-between items-center text-primary pb-6 xl:pb-10 px-[10%]">
+      <div className="flex flex-col items-center lg:items-start lg:w-[250px] xl:w-[350px]">
+        <a href="/" className="flex items-center gap-2 pt-6 ">
+          <img src={Logo} alt="logo" className="size-8" />
+          <h1 className="text-primary font-extrabold lg:text-xl xl:text-2xl">
+            Hydrocare
+          </h1>
+        </a>
+
+        <h1 className="text-lg font-medium text-center lg:text-left lg:text-base lg:pt-2 xl:text-lg pt-4">
+          Bersama Menciptakan Dunia dengan Air Bersih untuk Semua.
+        </h1>
+      </div>
+      
+      <div className="flex pt-8 gap-24 lg:gap-16 xl:gap-20">
         <div>
-          <h1 className="text-xl font-bold">Dukungan</h1>
-          <ul className="pt-3 flex flex-col gap-2">
+          <h1 className="text-xl font-bold xl:text-2xl">Dukungan</h1>
+          <ul className="pt-3 flex flex-col gap-2 lg:gap-1 xl:gap-3 xl:text-lg">
             <li>Tentang Kami</li>
             <li>Ketentuan</li>
             <li>Kebijakan Privasi</li>
           </ul>
         </div>
         <div>
-          <h1 className="text-xl font-bold">Informasi</h1>
-          <ul className="pt-3 flex flex-col gap-2">
+          <h1 className="text-xl font-bold xl:text-2xl">Informasi</h1>
+          <ul className="pt-3 flex flex-col gap-2 lg:gap-1 xl:gap-3 xl:text-lg">
             <li>Blog</li>
             <li>Contact</li>
             <li>Bantuan</li>
@@ -30,9 +39,9 @@ export default function Footer() {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-2xl pt-8 font-extrabold">Sosial Media Kami</h1>
-        <p className="font-medium text-xs">
-          &copy; 2024 Hydrocare All Rights Reserved{" "}
+        <h1 className="text-2xl pt-8 xl:pt-9 font-extrabold xl:text-3xl lg:pt-10">Sosial Media Kami</h1>
+        <p className="font-medium text-xs xl:text-base">
+          &copy; 2024 Hydrocare All Rights Reserved
         </p>
         <div className="flex py-4 gap-2">
           <a href="">
