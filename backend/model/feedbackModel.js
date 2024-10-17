29b621462d36eaacb114a54682c1fd7813e2835a
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-10-17 17:48:19 +0700

    Add feedback model and routes

---
+++ backend/model/feedbackModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const feedbackSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Feedback = mongoose.model("Feedback", feedbackSchema)
